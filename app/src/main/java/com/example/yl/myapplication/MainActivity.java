701985e7441a2770dfd8032aef9f29bf2cbef867
Unknown <fengwu2006@gmail.com>
2018-02-01 12:55:47 +0800

    save

--- app/src/main/java/com/example/yl/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yl/myapplication/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.yl.myapplication;
 
+import android.icu.text.RelativeDateTimeFormatter;
+import android.icu.text.SimpleDateFormat;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TimeUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+
+import java.security.Guard;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,33 +53,69 @@ public class MainActivity extends AppCompatActivity {
   @OnClick(R.id.button)
   public void onClick(View v) {
 
-    Runnable runnable = new Runnable() {
-      @Override
-      public void run() {
+    ExecutorService es = Executors.newSingleThreadExecutor();
 
-        try {
+    CountRunnable work = new CountRunnable();
 
-          Thread.sleep(5000);
+    Future<Number> future = es.submit(work);
 
-          mTextView.post(new Runnable() {
-            @Override
-            public void run() {
+    System.out.println("任务开始于");
 
-              mTextView.setText("AS");
-            }
-          });
+    try {
 
-        }
-        catch (InterruptedException e) {
+      TimeUnit.SECONDS.sleep(1);
 
-          e.printStackTrace();
-        }
-      }
-    };
+      System.out.println("主线程" + Thread.currentThread().getName() + "依然可以执行");
+    }
+    catch (InterruptedException e) {
 
-    Thread thread = new Thread(runnable);
+      e.printStackTrace();
+    }
 
-    thread.start();
+    try {
+
+      Number object = future.get();
+
+      System.out.println("任务结束于" + " result="+object.num);
+    }
+    catch (Exception e) {
+
+      e.printStackTrace();
+    }
+
+    es.shutdown();
+
+    System.out.println("任务结束"+es.isShutdown());
+  }
+
+  static class Number{
+
+    private int num;
+
+    private int getNum(){
+
+      return num;
+    }
+
+    private void setNum(int num){
+
+      this.num = num;
+    }
+  }
+
+  private class CountRunnable implements Callable<Number> {
+
+    @Override
+    public Number call() throws Exception {
+
+      Number number = new Number();
+
+      TimeUnit.SECONDS.sleep(2);
+
+      number.setNum(10);
+
+      return number;
+    }
   }
 
   @Override
